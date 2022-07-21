0213e08b6b7c4fc4c8dc17f60f8f7a337ae1a626
Elijah Mendoza <elijah@parentschooling.com>
2022-07-21 20:55:48 +0800

    The last dance

--- api/_lib/parser.ts
+++ api/_lib/parser.ts
@@ -52,13 +52,13 @@ function getArray(stringOrArray: string[] | string | undefined): string[] {
 
 function getDefaultImages(images: string[], theme: Theme): string[] {
     const defaultImage = theme === 'light'
-        ? 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg'
-        : 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg';
+        ? 'https://blog.elijahmendoza.nom.za/brand/logo-light-transparent.svg'
+        : 'https://blog.elijahmendoza.nom.za/brand/logo-dark-transparent.svg';
 
     if (!images || !images[0]) {
         return [defaultImage];
     }
-    if (!images[0].startsWith('https://assets.vercel.com/') && !images[0].startsWith('https://assets.zeit.co/')) {
+    if (!images[0].startsWith('https://blog.elijahmendoza.nom.za/')) {
         images[0] = defaultImage;
     }
     return images;
